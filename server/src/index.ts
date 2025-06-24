f923c5bfcb02b8c074d570f8e03e9a674f196609
0xShravanth <shravanth0023@gmail.com>
2025-06-24 12:38:31 +0530

    addded index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,45 @@
-import { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 /* Route Imports */
+
+/* configuration */
+dotenv.config();
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+/*cors configuration */
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN ||
+      "*" || ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+    methods: ["GET", "Post", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/*Routs */
+app.get("/", (req, res) => {
+  res.send("this is home route");
+});
+
+app.use("/projects");
+app.use("/tasks");
+app.use("/search");
+app.use("/users");
+app.use("teams");
+
+/*server */
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
